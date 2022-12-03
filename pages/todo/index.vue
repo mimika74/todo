a02c88a9224b777eba7b3759853aa7bbfb0b4cf3
steeq10 <thfoeichir74mcs@gmail.com>
2022-12-03 19:39:12 +0900

    Initial Commit

---
+++ pages/todo/index.vue
@@ -0,0 +1,19 @@
+<script setup lang='ts'>
+interface todo {
+  message: string
+}
+const message = ref('')
+const checked = ref(false)
+
+</script>
+
+<template>
+  <div>
+    工事中...
+    <textarea type='text' v-model='message'></textarea>
+    <p>{{ message }}</p>
+    <input type='checkbox' id='checkbox' v-model='checked' />
+    <label for='checkbox'>{{ checked }}</label>
+  </div>
+
+</template>
