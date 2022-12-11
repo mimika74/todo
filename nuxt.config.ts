17dccd8ff5bf16d1be659417969eb2e66d40e0eb
steeq10 <thfoeichir74mcs@gmail.com>
2022-12-11 18:10:13 +0900

    [add]firestoreからgetDocsする

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import { config } from 'dotenv'
-const envPath = `.env.${process.env.APP_ENV}`
-config({ path: envPath })
-
 export default defineNuxtConfig({
   app: {
     head: {
-      title: 'toodoo',
+      title: 'todo',
       meta: [
         { charset: 'utf-8' },
         { 'http-equiv': 'x-ua-compatible', content: 'IE=edge' },
         { name: 'viewport', content: 'width-device-width, initial-scale=1.0' },
       ],
+    },
+  },
+  typescript: {
+    strict: true,
+  },
+  ssr: false,
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      }
     }
   },
   runtimeConfig: {
     public: {
       firebaseConfig: {
-          apiKey: process.env.FIREBASE_API_KEY,
-          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-          messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-          appId: process.env.FIREBASE_APP_ID,
-          measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
       }
     }
   },
---
+++ pages/main/index.vue
@@ -0,0 +1,101 @@
+<script setup lang='ts'>
+import { getFirestore, collection } from 'firebase/firestore'
+import { CollectionReference, doc, getDoc, getDocs } from '@firebase/firestore'
+import { initializeApp } from "firebase/app";
+
+//firebaseと接続する
+const runtimeConfig = useRuntimeConfig()
+const firebaseConfig = {
+  apiKey: runtimeConfig.public.firebaseConfig.apiKey,
+  authDomain: runtimeConfig.public.firebaseConfig.authDomain,
+  projectId: runtimeConfig.public.firebaseConfig.projectId,
+  storageBucket: runtimeConfig.public.firebaseConfig.storageBucket,
+  messagingSenderId: runtimeConfig.public.firebaseConfig.messagingSenderId,
+  appId: runtimeConfig.public.firebaseConfig.appId,
+}
+const firebaseApp = initializeApp(firebaseConfig)
+const firestore = getFirestore(firebaseApp)
+
+//⓪取得するデータの方を用意する
+type Task = {
+  category: string
+  person: string
+  title: string
+  detail: string
+}
+
+//①firestoreからデータを取得する
+const TaskRef = collection(
+    firestore,
+    'tasks'
+) as CollectionReference<Task>
+
+//②一つのドキュメントを取得する時
+//const TaskRefId1 = doc(TaskRef, 'upuO8Oq5sLxzQd0p4uKg')
+//const tasksDocId1 = await getDoc(TaskRefId1)
+//const tasksDataId1 = tasksDocId1.data()
+//console.log(tasksDataId1)
+
+//②複数のドキュメントを取得する時
+const tasksDocs = await getDocs(TaskRef)
+const tasksAllData = tasksDocs.docs.map((doc) => doc.data())
+//console.log(tasksAllData)
+
+
+//データを追加する時に使う
+const task = ref<Task>({
+  category: '',
+  title: '',
+  detail: '',
+  person: '',
+})
+
+const onSubmit = async () => {
+  //await addTask(task)
+}
+
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent='onSubmit'>
+      <div>カテゴリー<input type='text' v-model='task.category' /></div>
+      <div>タイトル<input type='text' v-model='task.title' /></div>
+      <div>詳細<input type='text' v-model='task.detail' /></div>
+      <div>担当者<input type='text' v-model='task.person' /></div>
+      <button>保存する</button>
+    </form>
+    <table>
+      <tbody>
+      <tr>
+        <th>
+          カテゴリー
+        </th>
+        <th>
+          タイトル
+        </th>
+        <th>
+          詳細
+        </th>
+        <th>
+          担当者
+        </th>
+      </tr>
+      <tr v-for='task in tasksAllData'>
+        <td>
+          {{ task.category }}
+        </td>
+        <td>
+          {{ task.title }}
+        </td>
+        <td>
+          {{ task.detail }}
+        </td>
+        <td>
+          {{ task.person }}
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
